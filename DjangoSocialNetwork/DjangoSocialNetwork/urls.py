8a505e46dc1989042a710656c7e6abde0d109307
Pedro L <pedrolucena@protonmail.com>
2022-05-11 18:44:09 -0300

    Added base.html and navbar + static files

--- DjangoSocialNetwork/DjangoSocialNetwork/urls.py
+++ DjangoSocialNetwork/DjangoSocialNetwork/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main import views
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -23,4 +24,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.index, name='index'),
     path('index', views.index, name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
