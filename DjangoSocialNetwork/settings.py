77742690714f9403d9dba57770b7b563acc51ec6
Pedro L <pedrolucena@protonmail.com>
2022-07-06 14:40:03 -0300

    Update settings.py

--- DjangoSocialNetwork/settings.py
+++ DjangoSocialNetwork/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -142,7 +142,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL='/login'
 LOGIN_REDIRECT_URL='/login'
 
-django_heroku.settings(locals())
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'hodpd2mtw',
@@ -187,3 +186,5 @@ LOGGING = {
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+
+django_heroku.settings(locals())
