501b38af6f5b683a6ff04662e186212e71bad90e
Pedro L <pedrolucena@protonmail.com>
2022-07-05 22:58:56 -0300

    Changed settings.py

--- DjangoSocialNetwork/settings.py
+++ DjangoSocialNetwork/settings.py
@@ -128,14 +128,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-import os
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
