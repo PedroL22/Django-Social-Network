52ac7eaaecf7a68e984eaed01070c3f707721bcd
Pedro L <pedrolucena@protonmail.com>
2022-07-06 13:58:50 -0300

    Update settings.py

--- DjangoSocialNetwork/settings.py
+++ DjangoSocialNetwork/settings.py
@@ -150,3 +150,21 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': 'eVdaYIwgdOOCZuKAgQe_9l4DVG8',
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+import logging
+import os
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
