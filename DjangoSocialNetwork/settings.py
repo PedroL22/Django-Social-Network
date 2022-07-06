edb248a93e5079204a98abc9ed1fc63b9b9aa96a
Pedro L <pedrolucena@protonmail.com>
2022-07-06 14:27:29 -0300

    Update settings.py

--- DjangoSocialNetwork/settings.py
+++ DjangoSocialNetwork/settings.py
@@ -132,9 +132,6 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_USE_FINDERS = True
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
