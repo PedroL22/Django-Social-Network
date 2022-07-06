b19572795377a8013b45aa3bad41ab9f1272b843
Pedro L <pedrolucena@protonmail.com>
2022-07-05 22:42:22 -0300

    Update settings.py

--- DjangoSocialNetwork/settings.py
+++ DjangoSocialNetwork/settings.py
@@ -128,7 +128,16 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
