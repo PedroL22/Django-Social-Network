52327441dfe412c6a343edeb8e17fc6c1788f664
Pedro L <pedrocontact22@gmail.com>
2022-07-22 18:00:19 -0300

    Author id href

--- main/forms.py
+++ main/forms.py
@@ -57,7 +57,7 @@ class PasswordChangingForm(PasswordChangeForm):
 class AddCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('text','img')
+        fields = ('text',)
 
         widgets = {
             'text': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Comment'}),
